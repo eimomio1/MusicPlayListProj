494f7931888f5ce34a67966b771312ccc34741e8
unknown <imomiom627@gmail.com>
2023-11-02 20:18:03 -0400

    Added Entity Classes and many more

---
+++ src/main/java/com/proj/music/entity/Genre.java
@@ -0,0 +1,53 @@
+package com.proj.music.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "genre_tbl")
+public class Genre {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "genre_id")
+	private long id;
+	@Column(name = "name")
+	private String name;	
+	@Column(name = "description")
+	private String description;
+	public Genre() {
+		super();
+	}
+	public Genre(long id, String name, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	@Override
+	public String toString() {
+		return "Genre [id=" + id + ", name=" + name + ", description=" + description + "]";
+	}
+}
+
--- src/main/java/com/proj/music/entity/Song.java
+++ src/main/java/com/proj/music/entity/Song.java
@@ -4,36 +4,38 @@ import java.time.LocalDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table
+@Table(name = "songs")
 public class Song {
 	@Id
 	@Column(name = "song_id", nullable = false)
-	private int id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
 	@Column(name ="name")
 	private String name;
 	@Column(name = "duration")
 	private double duration;
-	@Column(name = "releaseDate")
+	@Column(name = "release_date")
 	private LocalDate releaseDate;
 	public Song() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
-	public Song(int id, String name, double duration, LocalDate releaseDate) {
+	public Song(long id, String name, double duration, LocalDate releaseDate) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.duration = duration;
 		this.releaseDate = releaseDate;
 	}
-	public int getId() {
+	public long getId() {
 		return id;
 	}
-	public void setId(int id) {
+	public void setId(long id) {
 		this.id = id;
 	}
 	public String getName() {
